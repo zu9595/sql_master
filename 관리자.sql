eab6b544a86a3ba010122e12c8482adbf6a158be
qkrwldnd <>
2024-01-15 21:46:54 +0900

    240115

---
+++ 관리자.sql
@@ -0,0 +1,21 @@
+alter user dev
+identified by dev
+account unlock;
+
+create user dev
+identified by test;
+
+grant create session, create table, create view
+to dev;
+
+revoke create session, create table, create view
+from dev;
+
+create user dev
+identified by dev
+default tablespace users
+temporary tablespace temp;
+------------------------------
+create user mun9test identified by mun9test
+default tablespace users;
+grant connect, resource to mun9test;
